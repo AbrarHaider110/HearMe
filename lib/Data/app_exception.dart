8456cdecc2c842e3dcd8656ff62ed644c0f9854c
Abrar Haider <abrarhaider104@gmail.com>
2025-12-09 09:27:13 +0500

    Updated

--- lib/Data/app_exception.dart
+++ lib/Data/app_exception.dart
@@ -1,11 +1,12 @@
 class AppException implements Exception {
-  final _message;
-  final _prefix;
+  final String? message;
+  final String? prefix;
 
-  AppException(this._message, this._prefix);
+  const AppException([this.message, this.prefix]);
 
+  @override
   String toString() {
-    return '$_message$_prefix';
+    return '$prefix$message';
   }
 }
 
